b65886e58569813a299fc0c05eabcdc4b57732ac
alearteaga <159954253+alearteaga@users.noreply.github.com>
2024-06-02 17:34:08 +0200

    nuevo

--- src/biblioteca/DatabaseConnection.java
+++ src/biblioteca/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/BibliotecaCanCasacuberta";
+    private static final String URL = "jdbc:mysql://localhost:3306/biblioteca";
     private static final String USER = "root";
-    private static final String PASSWORD = "yourpassword";
+    private static final String PASSWORD = "My7Pass@Word_9_8A_zE";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
